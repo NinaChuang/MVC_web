aba07fab5655e7b4b463326a7b517e0a28c6f106
Homda <Homda@HTLienn>
2016-11-24 20:19:39 +0800

    zz

--- MVC_web/src/com/acer/mvc/controller/ControllerServletImpl.java
+++ MVC_web/src/com/acer/mvc/controller/ControllerServletImpl.java
@@ -34,16 +34,12 @@ public class ControllerServletImpl extends HttpServlet implements IController {
 	}
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String text = "some text";
 		
 		String msg=request.getParameter("msg");
-		System.out.println(text+","+msg);
-		response.setContentType("text/plain"); // Set content type of the
-												// response so that jQuery knows
-												// what it can expect.
-		response.setCharacterEncoding("UTF-8"); // You want world domination,
-												// huh?
-		response.getWriter().write(text+","+msg);
+		
+		response.setContentType("text/plain"); 		
+		response.setCharacterEncoding("UTF-8"); 			
+		response.getWriter().write(msg);
 		
 	}
 }
