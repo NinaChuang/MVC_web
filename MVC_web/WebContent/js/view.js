78f77c8676473765c1dbebffdc7ce0f174fd427a
spadem45420 <spadem45420@gmail.com>
2016-11-30 11:10:31 +0800

    解衝突

--- MVC_web/WebContent/js/view.js
+++ MVC_web/WebContent/js/view.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
+	upLoadFile();
 	setCityOption();
 	doOnchageCity();
+//	setTownOption();
+});
+
+$(document).ready(function() {
+	setSubmitOption();
+	doOnclickCity();
 });
 
 // 查詢下拉式選項
@@ -10,20 +17,47 @@ function setCityOption() {
 	};
 	$.ajax({
 		url : "controllerServletImpl", //to controller path
-		type : "POST",                 //doPost
+		type : "POST", //doPost
 		cache : false,
-		data : JSON.stringify(json),   //send data source
+		data : JSON.stringify(json), //send data source
 		async : false,
 		// dataType : "'text json'",
 		contentType : "application/json; charset=utf-8", //data type
 		success : function(response) {
 			var json = JSON.parse(response);
-			doSetAttributeOption(json);      //if method success, do this method.
+			doSetAttributeOption(json); //if method success, do this method.
 		},
 		error : function(xhr) {
 			alert(xhr.status);
 		},
 	});
+
+	function setSubmitOption() {
+        var email = $("#email").val();
+        var phone = $("#phone").val();
+		var json = {
+            'submit' : 'submit',
+			'email' : email ,
+			'phone' : phone
+		};
+		$.ajax({
+			url : "controllerServletImpl", //to controller path
+			type : "POST", //doPost
+			cache : false,
+			data : JSON.stringify(json), //send data source
+			async : false,
+			// dataType : "'text json'",
+			contentType : "application/json; charset=utf-8", //data type
+			success : function(response) {
+				var json = JSON.parse(response);
+				doSetAttributeOption(json); //if method success, do this method.
+			},
+			error : function(xhr) {
+				alert(xhr.status);
+			},
+		});
+	}
+
 }
 // 將資料塞進Attribute下拉式選項中
 function doSetAttributeOption(json) {
@@ -33,7 +67,12 @@ function doSetAttributeOption(json) {
 	for (var i = 1; i < AttributeCount; i++) {
 		Attribute.remove(1);
 	}
-
+	
+	var option = document.createElement("option");
+	option.value = "請選擇";
+	option.text = "請選擇";
+	Attribute.add(option);
+	
 	for (var i = 0; i < json.length; i++) {
 		var option = document.createElement("option");
 		option.value = json[i]["provinceNo"] + ',' + json[i]["cityNo"];
@@ -46,11 +85,28 @@ function doSetAttributeOption(json) {
 function setTownOption(json) {
 	var Attribute = document.getElementById("addressTown");
 	var AttributeCount = Attribute.length;
+	
+//	if(AttributeCount > 0) {
+//		$('#addressTown option').remove();
+//	}
 
 //	for (var i = 1; i < AttributeCount; i++) {
+//		Attribute.remove(0);
+//	}
+	
+//	for (var i = AttributeCount -1 ; i >= 0 ; i--) {
 //		Attribute.remove(1);
 //	}
 
+	for (var i = 0; i < AttributeCount; i++) {
+		Attribute.remove(0);
+	}
+
+//	var option = document.createElement("option");
+//	option.value = "請選擇";
+//	option.text = "請選擇";
+//	Attribute.add(option);
+	
 	for (var i = 0; i < json.length; i++) {
 		var option = document.createElement("option");
 		option.value = json[i]["provinceNo"] + ',' + json[i]["cityNo"] + ','
@@ -69,7 +125,7 @@ function doOnchageCity() {
 			'getTown' : msg
 		};
 
-		alert(msg);
+//		alert(msg);
 		$.ajax({
 			url : "controllerServletImpl",
 			type : "POST",
@@ -84,14 +140,39 @@ function doOnchageCity() {
 			},
 			error : function(xhr) {
 				alert(xhr.status);
-
 			},
 
 		});
 
 	});
 }
+function upLoadFile() {
+	$('#submitBtn').on('click', function() {
+//		$.getScript("js/ajaxfileupload.js", function() {
 
+			$.ajaxFileUpload({
+				url : 'contorllerFielUpload',
+				secureuri : false,
+				fileElementId : 'exampleInputFile',
+				dataType : 'json',
+				success : function(data, status) {
+					if (typeof (data.error) != 'undefined') {
+						if (data.error != '') {
+							alert(data.error);
+						} else {
+							alert(data.msg);
+						}
+					}
+				},
+				error : function(data, status, e) {
+					alert(e);
+				}
+			})
+
+//		});
+
+	});
+}
 // $(document).on("click", "#submit", function(event) {
 
 // var msg = $('#msg').val();
